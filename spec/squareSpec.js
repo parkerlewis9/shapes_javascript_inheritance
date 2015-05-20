ea0f6831f7d0ee591145ab101121faa82a27598b
Parker Lewis <parkerlewis@Parkers-MacBook-Air.local>
2015-05-20 15:56:25 -0700

    finish square spec

--- spec/squareSpec.js
+++ spec/squareSpec.js
@@ -1,10 +1,12 @@
 var Square = require("../src/square");
 
 var square;
+var square2;
 
 describe("Square", function() {
   beforeEach(function() {
     square = new Square(2, "red");
+    square2 = new Square(0, "red");
   });
 
   describe("Area", function() {
@@ -13,14 +15,17 @@ describe("Square", function() {
     });
 
     it("should be 0 for square2", function() {
-      var square2 = new Square(0, "red");
+      expect(square2.area()).toEqual(0);
     });
   });
-  
+
   describe("Perimeter", function() {
     it("should be 8 for a square with side length 2", function() {
       expect(square.perimeter()).toEqual(8);
     });
+    it("should be 0 for square2", function() {
+      expect(square2.perimeter()).toEqual(0);
+    })
   });
 
 
