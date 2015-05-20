f4871c6eb256d6c32eb8aeb4ee2cb20da09a4bb8
Parker Lewis <parkerlewis@Parkers-MacBook-Air.local>
2015-05-20 15:52:49 -0700

    more square specs

--- spec/squareSpec.js
+++ spec/squareSpec.js
@@ -11,13 +11,18 @@ describe("Square", function() {
     it("should be 4 for a square", function() {
       expect(square.area()).toEqual(4);
     });
-  });
 
-  describe("Perimeter", function() {
-    it("should be 8 for a square with side length 2", function() {
-      expect(square.area()).toEqual(8);
+    it("should be 0 for square2", function() {
+      var square2 = new Square(0, "red");
     });
   });
+  
+  describe("Perimeter", function() {
+    it("should be 8 for a square with side length 2", function() {
+      expect(square.perimeter()).toEqual(8);
+    });
+  });
+
 
   // Write more specs!!
 });
--- src/square.js
+++ src/square.js
@@ -4,13 +4,17 @@ function Square(sideLength, color) {
   Shape.call(this, 4, color);
   this.sideLength = sideLength;
 }
-
 // TODO: Implement area, perimeter, draw and toString for Square.
 // DO NOT reimplement getRGB.  That should only have to be done once.
 Square.prototype = Object.create(Shape.prototype);
 Square.prototype.constructor = Square;
 
-module.exports = Square;
+Square.prototype.area = function() {
+	return this.sideLength * this.sideLength; 
+};
 
-var square = new Square(2, "red");
-console.log(square.area())
+Square.prototype.perimeter  = function() {
+	return 4 * this.sideLength;
+}
+
+module.exports = Square;
